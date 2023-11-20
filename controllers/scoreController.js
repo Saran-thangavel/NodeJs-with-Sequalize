9cd73d2fcd53be4d91acc03e7dd142740f4435cf
Saran-thangavel <tgsaran6@gmail.com>
2023-11-20 19:01:31 +0530

    score api completed

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -4,43 +4,44 @@ const getTokenInfo = require("../utils/tokenInfo");
 const answersModal = require("../models/UserAnswer");
 
 const getUserScore = async (req, res) => {
-    var result;
     try {
         const response = getTokenInfo(req, res);
         const userId = response.userId;
         console.log(userId);
         const questions = await questionModal.findAll();
 
-        if (!questions) {
+        if (!questions || questions.length === 0) {
             return res.status(400).json({ message: "No questions found." });
         }
 
         const answers = await answersModal.findAll({ where: { user_id: userId } });
         let score = 0;
-        answers.forEach((data) => {
-            const question = optionModal.findAll({ where: { question_id: data.ques_id } });
-            console.log("inside");
-            if (question) {
-                const correctAnswerId = question.correct_answer;
+
+        for (const data of answers) {
+            const question = await optionModal.findAll({ where: { question_id: data.ques_id } });
+
+            if (question && question.length > 0) {
+                const correctAnswerId = question.find((option) => option.correct_answer === true).id;
+
                 if (data.chosen_answer === correctAnswerId) {
-                    return score++;
+                    score++;
                 }
             }
-            console.log(score, "score");
+        }
 
-            const answeredQuestionsData = unAnsweredQuestions(questions, answers);
+        const answeredQuestionsData = unAnsweredQuestions(questions, answers);
 
-            console.log(unAnsweredQuestions, "amswer");
-            result = {
-                score: `${score}/${questions.length}`,
-                unAnsweredQuestionCount: questions.length - answeredQuestionsData,
-            };
-        });
+        const result = {
+            score: `${score}/${questions.length}`,
+            unAnsweredQuestionCount: questions.length - answeredQuestionsData,
+        };
+
         return res.status(200).json({ message: result });
     } catch (error) {
         return res.status(500).json({ message: error });
     }
 };
+
 
 const unAnsweredQuestions = (questions, answers) => {
     var answeredQuestions = 0;
